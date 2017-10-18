4d5cf8e9072c7ecb889335d8f9e13e436c630c34
Veriqus <android@veriqus.com>
2017-10-18 22:11:35 +0200

    connection test commit

--- app/src/main/java/com/veriqus/savoirvivre/ModeFragment.java
+++ app/src/main/java/com/veriqus/savoirvivre/ModeFragment.java
@@ -31,6 +31,7 @@ public class ModeFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
