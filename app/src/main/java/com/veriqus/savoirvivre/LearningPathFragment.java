728cc6d182adbd442e2d11d3fa4996a0c98a0fac
Veriqus <android@veriqus.com>
2017-12-05 20:42:54 +0100

    quiz layout

--- app/src/main/java/com/veriqus/savoirvivre/LearningPathFragment.java
+++ app/src/main/java/com/veriqus/savoirvivre/LearningPathFragment.java
@@ -26,6 +26,7 @@ public class LearningPathFragment extends Fragment {
     View rootView;
     SubCatListAdapter.OnLearningSubSelected mCallback;
     boolean isDone;
+    final List<SubClass> subClassList = new ArrayList<>();
 
 
     public LearningPathFragment() {
@@ -48,74 +49,7 @@ public class LearningPathFragment extends Fragment {
         Bundle bundle = getArguments();
         int category = bundle.getInt("CATEGORY");
 
-        final List<SubClass> subClassList = new ArrayList<>();
-        switch (category){
-            case R.string.category_1:
-                subClassList.add(new SubClass("Pisanie wiadomości", isDone));
-                subClassList.add(new SubClass("Odbieranie telefonów"));
-                subClassList.add(new SubClass("Używanie telefonu w miejscach publicznych"));
-                subClassList.add(new SubClass("Początek maila"));
-                subClassList.add(new SubClass("Rozwinięcie maila"));
-                subClassList.add(new SubClass("Zakończenie maila"));
-                subClassList.add(new SubClass("Sklepy i aukcje internetowe"));
-                subClassList.add(new SubClass("Komentarze w internecie"));
-                subClassList.add(new SubClass("eSport"));
-                break;
-            case R.string.category_2:
-                subClassList.add(new SubClass("Szwedzki stół"));
-                subClassList.add(new SubClass("Kontakt z kelnerem"));
-                subClassList.add(new SubClass("Napiwki"));
-                subClassList.add(new SubClass("Zastawa"));
-                subClassList.add(new SubClass("Sztućce"));
-                subClassList.add(new SubClass("Serwowanie alkoholi"));
-                break;
-            case R.string.category_3:
-                subClassList.add(new SubClass("Wygląd i prezencja"));
-                subClassList.add(new SubClass("Powitania"));
-                subClassList.add(new SubClass("Pożegnania"));
-                subClassList.add(new SubClass("Precedencja"));
-                subClassList.add(new SubClass("Tytułowanie"));
-                subClassList.add(new SubClass("Przedstawianie"));
-                subClassList.add(new SubClass("Randka"));
-                subClassList.add(new SubClass("Kwiaty"));
-                subClassList.add(new SubClass("Prezenty"));
-                subClassList.add(new SubClass("Rozmowy biznesowe"));
-                subClassList.add(new SubClass("Rozmowy z nieznajomymi"));
-                subClassList.add(new SubClass("Taniec"));
-                subClassList.add(new SubClass("Niepełnosprawni"));
-                subClassList.add(new SubClass("Goście w domu"));
-                subClassList.add(new SubClass("Zaproszenia"));
-                subClassList.add(new SubClass("Papierosy"));
-                break;
-            case R.string.category_4:
-                subClassList.add(new SubClass("Ślub"));
-                subClassList.add(new SubClass("Wesele"));
-                subClassList.add(new SubClass("Pogrzeb"));
-                subClassList.add(new SubClass("Stypa"));
-                break;
-            case R.string.category_5:
-                subClassList.add(new SubClass("Sklep"));
-                subClassList.add(new SubClass("Hotel"));
-                subClassList.add(new SubClass("Szpital, przychodnia"));
-                subClassList.add(new SubClass("Plaża"));
-                subClassList.add(new SubClass("Pasażer"));
-                subClassList.add(new SubClass("Kierowca"));
-                subClassList.add(new SubClass("Na statku"));
-                subClassList.add(new SubClass("W samolocie"));
-                subClassList.add(new SubClass("Anglia"));
-                subClassList.add(new SubClass("Japonia"));
-                subClassList.add(new SubClass("Siłownia"));
-                subClassList.add(new SubClass("Sauna"));
-                subClassList.add(new SubClass("Miejsca religijne"));
-                subClassList.add(new SubClass("Teatr, opera, kino, koncert"));
-                break;
-            case R.string.category_6:
-                break;
-            case R.string.category_7:
-                break;
-
-        }
-
+        generateSubCategories(category);
 
         RecyclerView rc = (RecyclerView) rootView.findViewById(R.id.rvSub);
         SubCatListAdapter adapter = new SubCatListAdapter(getContext(), subClassList);
@@ -135,6 +69,84 @@ public class LearningPathFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    private void generateSubCategories(int category){
+        switch (category){
+            case R.string.category_1:
+                subClassList.add(new SubClass("Pisanie wiadomości tekstowych", isDone));
+                subClassList.add(new SubClass("Odbieranie telefonów"));
+                subClassList.add(new SubClass("Używanie telefonu w miejscach publicznych"));
+                subClassList.add(new SubClass("Początek maila"));
+                subClassList.add(new SubClass("Rozwinięcie maila"));
+                subClassList.add(new SubClass("Zakończenie maila"));
+                subClassList.add(new SubClass("Media społecznościowe"));
+                subClassList.add(new SubClass("Sklepy i aukcje internetowe"));
+                subClassList.add(new SubClass("Komentarze w internecie"));
+                subClassList.add(new SubClass("eSport"));
+                break;
+            case R.string.category_2:
+                subClassList.add(new SubClass("Szwedzki stół"));
+                subClassList.add(new SubClass("Kontakt z kelnerem"));
+                subClassList.add(new SubClass("Napiwki"));
+                subClassList.add(new SubClass("Zastawa"));
+                subClassList.add(new SubClass("Sztućce"));
+                subClassList.add(new SubClass("Serwowanie alkoholi"));
+                break;
+            case R.string.category_3:
+                subClassList.add(new SubClass("Wygląd i prezencja"));
+                subClassList.add(new SubClass("Powitania"));
+                subClassList.add(new SubClass("Pożegnania"));
+                subClassList.add(new SubClass("Precedencja"));
+                subClassList.add(new SubClass("Tytułowanie"));
+                subClassList.add(new SubClass("Przedstawianie"));
+                subClassList.add(new SubClass("Sąsiedzi i współlokatorzy"));
+                subClassList.add(new SubClass("Randka"));
+                subClassList.add(new SubClass("Kwiaty"));
+                subClassList.add(new SubClass("Prezenty"));
+                subClassList.add(new SubClass("Rozmowy biznesowe"));
+                subClassList.add(new SubClass("Rozmowy z nieznajomymi"));
+                subClassList.add(new SubClass("Taniec"));
+                subClassList.add(new SubClass("Niepełnosprawni"));
+                subClassList.add(new SubClass("Goście w domu"));
+                subClassList.add(new SubClass("Zaproszenia"));
+                subClassList.add(new SubClass("Papierosy"));
+                break;
+            case R.string.category_4:
+                subClassList.add(new SubClass("Ślub i wesele"));
+                subClassList.add(new SubClass("Pogrzeb i stypa"));
+                subClassList.add(new SubClass("Uroczystości rodzinne - rocznice, urodziny, imieniny"));
+                break;
+            case R.string.category_5:
+                subClassList.add(new SubClass("Sklep"));
+                subClassList.add(new SubClass("Winda"));
+                subClassList.add(new SubClass("Hotel"));
+                subClassList.add(new SubClass("Szpital, przychodnia"));
+                subClassList.add(new SubClass("Plaża"));
+                subClassList.add(new SubClass("Pasażer"));
+                subClassList.add(new SubClass("Kierowca"));
+                subClassList.add(new SubClass("Na statku"));
+                subClassList.add(new SubClass("W samolocie"));
+                subClassList.add(new SubClass("Za granicą"));
+                subClassList.add(new SubClass("Siłownia"));
+                subClassList.add(new SubClass("Sauna"));
+                subClassList.add(new SubClass("Miejsca religijne"));
+                subClassList.add(new SubClass("Teatr, opera, kino, koncert"));
+                subClassList.add(new SubClass("Biblioteka i muzeum"));
+                break;
+            case R.string.category_6:
+                subClassList.add(new SubClass("Podstawy etykiety"));
+                subClassList.add(new SubClass("O savoir-vivrze"));
+                subClassList.add(new SubClass("Dla rodzica"));
+                subClassList.add(new SubClass("Zwierzęta"));
+                subClassList.add(new SubClass("Sport"));
+                subClassList.add(new SubClass("Recykling"));
+                break;
+            case R.string.category_7:
+                subClassList.add(new SubClass("Najczęstsze błędy językowe"));
+                subClassList.add(new SubClass("Stosowanie interpunkcji"));
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/veriqus/savoirvivre/QuizFragment.java
+++ app/src/main/java/com/veriqus/savoirvivre/QuizFragment.java
@@ -25,5 +25,4 @@ public class QuizFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz, container, false);
     }
-
 }
